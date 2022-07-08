f5b66a6bda56211651704aa8ffa2523649d11459
Benjamin V Rozonoyer <brozonoy@d720.bbn.com>
2022-07-08 19:20:13 -0400

    debug cnn dataloader

--- NeuralOceans.jl/src/dataloaders.jl
+++ NeuralOceans.jl/src/dataloaders.jl
@@ -23,22 +23,21 @@ function getdata(args)
     for (timestep, timestep_data) in data
 
         if args.fourier
-            RX, ΘX =  VectorPolarFromCartesian(fft(timestep_data[timestep]["rhs"]))  # shape 3600
-            push!(X, (RX, ΘX))
             if !args.cnn_input
-                Ry, Θy = VectorPolarFromCartesian(fft(reshape(timestep_data[timestep]["η"][3:64, 3:64], (62*62,))))  # shape (66, 66, 1) -> (62, 62, 1) -> 62*62=3844 strip away zeros and flatten
+                RX, ΘX =  VectorPolarFromCartesian(fft(timestep_data[timestep]["rhs"]))  # shape 3600
             else
-                Ry, Θy = VectorPolarFromCartesian(fft(timestep_data[timestep]["η"]))  # shape (66, 66, 1) assume padding 2 on each side to ignore zeros in input
+                RX, ΘX =  VectorPolarFromCartesian(fft(reshape(timestep_data[timestep]["rhs"], (60, 60, 1))))  # shape (60, 60, 1)
             end
+            push!(X, (RX, ΘX))
+            Ry, Θy = VectorPolarFromCartesian(fft(reshape(timestep_data[timestep]["η"][3:64, 3:64], (62*62,))))  # shape (66, 66, 1) -> (62, 62, 1) -> 62*62=3844 strip away zeros and flatten
             push!(y, (Ry, Θy))
 
         else
-            push!(X, timestep_data[timestep]["rhs"])  # shape 3600
             if !args.cnn_input
-                push!(y, reshape(timestep_data[timestep]["η"][3:64, 3:64], (62*62,)))    # shape (66, 66, 1) -> (62, 62, 1) -> 62*62=3844 strip away zeros and flatten
+                push!(X, timestep_data[timestep]["rhs"])  # shape 3600
             else
-                push!(y, timestep_data[timestep]["η"])    # shape (66, 66, 1) assume padding 2 on each side to ignore zeros in input
-            end
+                push!(X, reshape(timestep_data[timestep]["rhs"], (60, 60, 1)))  # shape (60, 60, 1)
+            push!(y, reshape(timestep_data[timestep]["η"][3:64, 3:64], (62*62,)))    # shape (66, 66, 1) -> (62, 62, 1) -> 62*62=3844 strip away zeros and flatten
         end
     end
     
