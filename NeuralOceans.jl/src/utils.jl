aa5fc44d11974531746b83f1ba336278d342292b
Benjamin V Rozonoyer <brozonoy@hdev-01.bbn.com>
2022-07-28 16:43:17 -0400

    confirmed Po vectorized Polar<->Cartesian

--- NeuralOceans.jl/src/utils.jl
+++ NeuralOceans.jl/src/utils.jl
@@ -1,5 +1,7 @@
 using CoordinateTransformations
 using ZippedArrays: ZippedArray
+using CUDA
+using Statistics
 
 
 function VectorPolarFromCartesian(Vcartesian)
@@ -13,4 +15,34 @@ function VectorCartesianFromPolar(VR, VΘ)
     CUDA.allowscalar() do
         return map(tup->tup[1]+tup[2]im, CartesianFromPolar().(map(tup->Polar(tup[1], tup[2]), ZippedArray(VR, VΘ))))
     end
+end
+
+
+function test_vectorized_coordinate_conversions()
+    
+    # create matrix of complex unit vectors
+    C = [1+0im          0+1im ;
+         1/√2+1/√2im    -√3/2-1im/2]
+
+    # convert to polar
+    R, Θ = VectorPolarFromCartesian(C)
+
+    # magnitudes should all be 1
+    @assert Statistics.mean(R) - 1 < 0.000001
+
+    # phases should equal Θ_gold
+    Θ_gold = [0     π ;
+              π/4  -5π/6]
+    @assert sum(Θ - Θ_gold) < 0.000001
+
+    # convert back to cartesian and make sure we're back at the starting point
+    Ĉ = VectorCartesianFromPolar(R, Θ)
+    δ = sum(C - Ĉ)
+    @assert √(real(δ)^2 + imag(δ)^2) < 0.000001
+
+end
+
+
+if abspath(PROGRAM_FILE) == @__FILE__
+    test_vectorized_coordinate_conversions()
 end
